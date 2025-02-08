37b328a4fc2dd16d5000056778f4a82e5d6e8081
Nitish Gidde <34066009+nitishnk@users.noreply.github.com>
2025-02-09 00:45:46 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -57,6 +57,6 @@ io.on("connection", (socket) => {
 });
 
 // Start the server
-server.listen(PORT, () => {
+server.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
