6d765643f3d41af8aba10b125be128cf4021e18b
Nitish Gidde <34066009+nitishnk@users.noreply.github.com>
2025-02-15 00:41:17 +0530

    Delete src directory

--- src/routes/postRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getPosts } = require('../controllers/postController');
-const { protect } = require('../middleware/authMiddleware');
-router.get('/',protect, getPosts);
-
-module.exports = router;
