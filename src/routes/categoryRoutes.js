0a8b80f0b0928eb774a1b6bd4d7f56d10ca3e764
Nitish Gidde <34066009+nitishnk@users.noreply.github.com>
2025-02-15 00:21:14 +0530

    Delete src directory

--- src/routes/categoryRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getCategories, addCategory } = require("../controllers/categoryController"); 
-const { protect } = require("../middleware/authMiddleware");
-
-router.get("/", protect, getCategories);
-router.post("/", protect, addCategory);
-
-module.exports = router;
